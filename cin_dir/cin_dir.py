dc2d1ae23bd7fa6217e48507645f119b65c14542
Stephan Brommer Gutiérrez <alu0101493497@ull.edu.es>
2024-12-25 13:33:14 +0000

    Update cin_dir.py

--- cin_dir/cin_dir.py
+++ cin_dir/cin_dir.py
@@ -15,7 +15,6 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import json
 import os
-import traceback
 
 # ******************************************************************************
 # Declaración de funciones
